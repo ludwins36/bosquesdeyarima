efc501933004f33ff0f238f8f731b8520f0ebf93
ludwins robles <ludwins36@gmail.com>
2019-06-25 12:14:14 -0500

    guardando

--- templates/tour_360.php
+++ templates/tour_360.php
@@ -12,19 +12,19 @@
  * @version 1.0
  */
 
-get_header('yarima'); ?>
+get_header('asul'); ?>
 
 <div class="">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			
-			<?php
-			while ( have_posts() ) : the_post();
 
-				get_template_part( 'template-parts/page/content', 'tour360' );
+			<?php
+			while (have_posts()) : the_post();
+
+				get_template_part('template-parts/page/content', 'tour360');
 
 				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
+				if (comments_open() || get_comments_number()) :
 					comments_template();
 				endif;
 
